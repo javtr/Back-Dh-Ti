fc791f557f89a896a480e863da73744404718bcc
javtr <javtriana.z@gmail.com>
2022-06-10 17:17:36 -0500

    mysql

---
+++ mysql_tut_01/src/main/java/com/example/mysql_tut_01/MysqlTut01Application.java
@@ -0,0 +1,13 @@
+package com.example.mysql_tut_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlTut01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlTut01Application.class, args);
+	}
+
+}
---
+++ mysql_tut_01/src/test/java/com/example/mysql_tut_01/MysqlTut01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysql_tut_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlTut01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
